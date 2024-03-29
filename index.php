a6f03900dd24fefbfc3bceb4fa274ae4ddfd4884
Dhanashri Gahiwal <gahiwald@gmail.com>
2024-03-29 12:17:35 +0530

    level 2 todo task one completed

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Todo</title>
+  </head>
+  <body>
+    <div class="container" id="container">
+      <!-- login screen -->
+      <div class="mb-3" id="profile">
+        <img src="./images/avatar.jpg" alt="profile picture">
+      </div>
+      <h1>Login To Add Tasks</h1>
+      <button type="button" class="btn btn-success py-2 px-4 mt-4">Login</button>
+    </div>
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  
+  </body>
+</html>
